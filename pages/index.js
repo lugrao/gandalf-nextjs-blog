37281dcb0c10cda4adcbc2fad756632244287d3b
lugrao <56491057+lagoru-q@users.noreply.github.com>
2020-04-24 15:12:18 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, fellow friend! I am here to help you throwing out your
+          jewellery into Mount Doom.
+        </p>
+        <p>
+          Please message me whenever you feel the burden is too
+          heavy.
+        </p>
+        <p>
+          <a href="https://lotr.fandom.com/wiki/Gandalf">Here</a> you can learn
+          more about me and my affairs.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.list}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
